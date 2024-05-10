d16904b53db6bc0fae374afec4442d33c12befe1
MaidedPonce <maided.h.p90@gmail.com>
2024-05-09 20:39:02 -0600

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Grid } from 'app/components'
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      Holi
+      <Grid />
+    </main>
+  )
+}
