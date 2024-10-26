90b90620e0fa3867b7ee8525a9e6b30de4324968
MaidedPonce <maided.h.p90@gmail.com>
2024-10-26 00:27:09 -0600

    Adding images

--- src/ui/containers/AboutMe.tsx
+++ src/ui/containers/AboutMe.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Image from 'next/image'
+
 const AboutMe = () => {
   return (
     <section className='bg-brand-purple text-white'>
@@ -17,6 +19,48 @@ const AboutMe = () => {
       </div>
       <div>
         <h2>Contactme</h2>
+        <ul className='flex max-w-96 justify-between gap-6 items-center'>
+          <li>
+            <a href='https://github.com/MaidedPonce'>
+              <Image
+                width={40}
+                height={40}
+                src='/icons/github.svg'
+                alt='Github'
+              />
+            </a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/maided-guadalupe-hernandez-ponce-5b2a2a1a5/'>
+              <Image
+                width={40}
+                height={40}
+                src='/icons/linkedin.png'
+                alt='LinkedIn'
+              />
+            </a>
+          </li>
+          <li>
+            <a href='mailto:maided.h.p90@gmail.com'>
+              <Image
+                width={40}
+                height={40}
+                src='/icons/emaill.png'
+                alt='Email'
+              />
+            </a>
+          </li>
+          <li>
+            <a href='https://wa.me/5536058161'>
+              <Image
+                width={40}
+                height={40}
+                src='/icons/wp.png'
+                alt='WhatsApp'
+              />
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   )
